c1e96ece699a2c1413bd928bedda19cf67aa553d
Aaron Tanguma <the_guma@yahoo.com>
2026-01-05 11:03:55 -0700

    completed CollectionContainsWord challenge

--- ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1794e0ea3c770ee2166a7d2f247ea06d79820f73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48c67a769bca2c1a3fe02e68e4b615fb8108fed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,7 +8,26 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            bool containsWord = false;
+
+            if (string.Equals(words, null) || words.Contains(null))
+            {
+                return false;
+            }
+            if (ignoreCase == true)
+            {
+                word = word.ToLower();
+                List<string> lowerCase = words.Select(x => x.ToLower()).ToList();
+
+                containsWord = lowerCase.Contains(word);
+            }
+
+            if (ignoreCase == false)
+            {
+                containsWord = words.Contains(word);
+            }
+
+            return containsWord;
         }
 
         public bool IsPrimeNumber(int num)
